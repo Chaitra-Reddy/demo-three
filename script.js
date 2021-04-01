304d293126e3b2a853b44918a52972c1f4272206
Chaitra Nuthanakalva <chaitra.nuthanakalva@mindtree.com>
2021-04-01 17:14:18 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+let data=[{
+    "name":"HTC Wildfire R70",
+    "manufacturer":"HTC",
+    "storage":"16",
+    "os":"android",
+    "camera":"8",
+    "price" : "48000",
+    "img" : "htc1.PNG"
+},
+{
+    "name":"Moto G Stylus",
+    "manufacturer":"Moto",
+    "storage":"32",
+    "os":"android",
+    "camera":"12",
+    "price" : "40000",
+    "img" : "moto1.PNG"
+},
+{
+    "name":"IPhone 12 Pro",
+    "manufacturer":"Apple",
+    "storage":"128",
+    "os":"ios",
+    "camera":"32",
+    "price" : "55000",
+    "img" : "iphone1.PNG"
+},
+{
+    "name":"HTC Wildfire X",
+    "manufacturer":"HTC",
+    "storage":"16",
+    "os":"android",
+    "camera":"8",
+    "price" : "38000",
+    "img" : "htc2.PNG"
+},
+{
+    "name":"Moto G Power",
+    "manufacturer":"Moto",
+    "storage":"32",
+    "os":"android",
+    "camera":"8",
+    "price" : "30000",
+    "img" : "moto2.PNG"
+},
+{
+    "name":"IPhone 12 Pro Max",
+    "manufacturer":"Apple",
+    "storage":"128",
+    "os":"ios",
+    "camera":"32",
+    "price" : "60000",
+    "img" : "iphone2.PNG"
+},
+{
+    "name":"HTC Desire 12",
+    "manufacturer":"HTC",
+    "storage":"16",
+    "os":"android",
+    "camera":"8",
+    "price" : "48000",
+    "img" : "htc3.PNG"
+},
+{
+    "name":"Moto G Play",
+    "manufacturer":"Moto",
+    "storage":"32",
+    "os":"android",
+    "camera":"12",
+    "price" : "36999",
+    "img" : "moto3.PNG"
+},
+{
+    "name":"IPhone 12 Mini",
+    "manufacturer":"Apple",
+    "storage":"64",
+    "os":"android",
+    "camera":"32",
+    "price" : "45000",
+    "img" : "iphone3.PNG"
+}]
